cfcddee09e06753a53437fce534867ed5a8e2423
Anastasiya <melichkaowen1@gmail.com>
2024-08-06 15:25:34 +0300

    feat: add swagger

--- TrafficLight/Program.cs
+++ TrafficLight/Program.cs
@@ -3,27 +3,39 @@ using TrafficLight.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Добавление сервисов в контейнер
 builder.Services.AddDbContext<TrafficLightContext>(options =>
 	options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Добавление Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+	c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "TrafficLight API", Version = "v1" });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Настройка HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
 	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.UseRouting();
+// Swagger middleware должно быть до UseRouting
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+	c.SwaggerEndpoint("/swagger/v1/swagger.json", "TrafficLight API v1");
+	c.RoutePrefix = string.Empty; // Swagger UI будет доступен по корню URL
+});
 
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
