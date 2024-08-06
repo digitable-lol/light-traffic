6f8b24c38a1e0f1ba836aa9bdfc720de50b74a4f
Anastasiya <melichkaowen1@gmail.com>
2024-08-06 17:15:18 +0300

    feat: add gitignore file

---
+++ application/TrafficLight/Models/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrafficLight.Models;
+
+public partial class Project
+{
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public DateOnly? EstimatedStartDate { get; set; }
+
+    public DateOnly? ActualStartDate { get; set; }
+
+    public int Id { get; set; }
+
+    public virtual ICollection<Report> Reports { get; set; } = new List<Report>();
+}
