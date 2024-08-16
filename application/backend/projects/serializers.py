9a9b8c941363e16dce59909c9126938f7dc5a11f
Vinogradov Anton <vino74974@gmail.com>
2024-08-16 23:04:56 +0300

    feature: Swagger; get all, get by id, create and delete methods for all views, views added to urls; fix: serializers structure

--- application/backend/projects/serializers.py
+++ application/backend/projects/serializers.py
@@ -1,34 +1,23 @@
 from rest_framework import serializers
-from .models import Employee
-class Project_Serializer(serializers.Serializer):
-    project_id = serializers.IntegerField()
-    project_name = serializers.CharField(max_length=200)
-    project_discription = serializers.CharField(max_length=500)
-    project_start_time = serializers.DateTimeField()
-    project_end_time = serializers.DateTimeField()
-    # employe = serializers.ManyRelatedField(Employee) Need to fix this part
+from .models import Employee, Objective, Project, Report
+class Project_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
 
-class Employee_Serializer(serializers.Serializer):
-    Employer_id = serializers.IntegerField()
-    first_name = serializers.CharField(max_length=50)
-    last_name = serializers.CharField(max_length=50)
+class Employee_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = '__all__'
 
-class Report_Serializer(serializers.Serializer):
-    report_id = serializers.ImageField()
-    report_name = serializers.CharField(max_length = 100)
-    report_start = serializers.DateTimeField()
-    report_finish = serializers.DateTimeField()
-    report_green_comment = serializers.CharField(max_length = 500)
-    report_yellow_comment = serializers.CharField(max_length = 500)
-    report_red_comment = serializers.CharField(max_length = 500)
-    employee_going_vacation = serializers.BooleanField()
-    anyone_going_vacation = serializers.CharField(max_length = 100)
-    other_comments = serializers.CharField(max_length = 1000)
-    related_project = serializers.IntegerField(source = 'Project.project_id')
-    report_creater = serializers.CharField(source = 'Employee.Employer_id')
+class Report_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Report
+        fields = '__all__'
+    
 
-class Objective_Serializer(serializers.Serializer):
-    objective_id = serializers.IntegerField()
-    objective_name = serializers.CharField(max_length = 100)
-    objective_color = serializers.CharField(max_length = 1)
-    related_report = serializers.IntegerField(source = 'Report.report_id')
+class Objective_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Objective
+        fields = '__all__'
+    
--- application/backend/projects/urls.py
+++ application/backend/projects/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
-
+from rest_framework.routers import DefaultRouter
 from . import views
 
 app_name = "projects"
 
-urlpatterns = [
-    path("",views.Project_List_API_View.as_view()),
-]
+router = DefaultRouter()
+router.register('employes', views.Employes_List_API_View, basename='employes')
+router.register('project', views.Project_List_API_View, basename='project')
+router.register('report', views.Report_List_API_View, basename='report')
+router.register('objective', views.Objective_List_API_View, basename='objective')
+urlpatterns = router.urls
