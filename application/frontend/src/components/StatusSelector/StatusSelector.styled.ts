aa1bac4f77a6fd1f4f991eabe4f9620363aa253e
Anastasiya <melichkaowen1@gmail.com>
2024-08-20 17:42:59 +0300

    feat: add status selector

---
+++ application/frontend/src/components/StatusSelector/StatusSelector.styled.ts
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material"
+import { styled } from "@mui/system"
+
+export const StatusCircle = styled(Box)({
+  width: 14,
+  height: 14,
+  borderRadius: "50%",
+  display: "inline-block",
+  marginRight: 8,
+})
+
+export const StyledFormControl = styled("div")({
+  width: "80px",
+})
