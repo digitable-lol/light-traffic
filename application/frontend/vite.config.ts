1f71da4e2e1d1e7258d6746205d69074bea21eb2
Yaroslav Vdovin <52864645+Proza1k@users.noreply.github.com>
2024-08-20 21:54:50 +0000

    fix: get assets with using another paths in url

--- application/frontend/vite.config.ts
+++ application/frontend/vite.config.ts
@@ -3,7 +3,7 @@ import tsconfigPaths from "vite-tsconfig-paths"
 
 import react from "@vitejs/plugin-react-swc"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-})
+  base: process.env.VITE_BASE_URL || '/'
+});
