e18f78e45a1089b4df79df32ed8bd753942a97a3
Vinogradov Anton <vino74974@gmail.com>
2024-08-21 21:52:48 +0300

    fix: delete unnecessary import

--- application/backend/projects/views.py
+++ application/backend/projects/views.py
@@ -1,7 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
-from django.db.models import Max
 
 from .models import Employee, Objective, Project, Report
 from .serializers import Employee_Serializer, Objective_Serializer, Project_Serializer, Report_Serializer
