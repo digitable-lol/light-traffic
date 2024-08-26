60e98284afee513eebf6db36b6b2a55e01c1f8ca
Anastasiya <melichkaowen1@gmail.com>
2024-08-26 14:39:58 +0300

    fix: change status selector

--- application/frontend/src/components/StatusSelector/StatusSelector.tsx
+++ application/frontend/src/components/StatusSelector/StatusSelector.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 
-import { MenuItem, Select } from "@mui/material"
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
+import { MenuItem, MenuProps, Select } from "@mui/material"
 
 import { StatusCircle, StyledFormControl } from "./StatusSelector.styled"
 
@@ -17,14 +18,36 @@ export const StatusSelector: React.FC<StatusSelectorProps> = ({ value, onChange 
     { value: "None", color: "gray" },
   ]
 
+  const MenuProps: Partial<MenuProps> = {
+    PaperProps: {
+      sx: {
+        borderRadius: "50px",
+        padding: "0px 0px 8px 0px",
+        boxShadow: "0px 6px 30px 5px rgba(0, 0, 0, 0.12)",
+      },
+    },
+  }
+
   return (
     <StyledFormControl>
       <Select
-        value={value}
+        value={value || "None"}
         onChange={(e) => onChange(e.target.value as string)}
         displayEmpty
         variant="outlined"
         inputProps={{ "aria-label": "Status Selector" }}
+        IconComponent={ExpandMoreIcon}
+        MenuProps={MenuProps}
+        sx={{
+          "& .MuiSelect-select": {
+            padding: "8px",
+            border: "none",
+            height: "auto",
+          },
+          "& .MuiOutlinedInput-notchedOutline": {
+            border: "none",
+          },
+        }}
       >
         {statusOptions.map((option) => (
           <MenuItem key={option.value} value={option.value}>
