3c848064e205ad10bfc81109ee6cba55c3e0ee37
Anastasiya <melichkaowen1@gmail.com>
2024-08-26 14:39:58 +0300

    fix: change status selector

--- application/frontend/src/components/StatusSelector/StatusSelector.styled.ts
+++ application/frontend/src/components/StatusSelector/StatusSelector.styled.ts
@@ -2,8 +2,8 @@ import { Box } from "@mui/material"
 import { styled } from "@mui/system"
 
 export const StatusCircle = styled(Box)({
-  width: 14,
-  height: 14,
+  width: 24,
+  height: 24,
   borderRadius: "50%",
   display: "inline-block",
   marginRight: 8,
