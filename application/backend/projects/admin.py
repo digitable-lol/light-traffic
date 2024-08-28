234ac6490433e56d7b7c9b15e71ce10409ae4f3a
Vinogradov Anton <vino74974@gmail.com>
2024-08-28 22:14:53 +0300

    fix: is_deleted fs hidden from the object creation form for all models

--- application/backend/projects/admin.py
+++ application/backend/projects/admin.py
@@ -5,16 +5,20 @@ from .models import Employee, Project, Report, Objective
 # Register your models here.
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
+    fields = ('project_name','project_description','project_start_time','project_end_time','employee')
     list_display = ("id","project_name", "project_start_time","project_end_time")
     search_fields = ('project_name','project_description','project_end_time','project_start_time','id')
     list_filter = ('project_start_time','project_end_time','employee')
 @admin.register(Report)
 class ReportAdmin(admin.ModelAdmin):
+    fields = ('report_name','report_creater','report_start','related_project','report_finish','report_green_comment','report_yellow_comment','report_red_comment','employee_going_vacation',
+              'anyone_going_vacation','other_comments','final_report_color')
     list_display = ("id","report_name", "report_start","report_finish","employee_going_vacation","report_creater","related_project","final_report_color")
     list_filter = ('report_creater',"related_project",'report_start','report_finish',"final_report_color","employee_going_vacation")
     search_fields = ('report_name','report_start','report_finish')
 @admin.register(Objective)
 class ObjectiveAdmin(admin.ModelAdmin):
+    fields = ('objective_name',"objective_descripion",'related_report','objective_color')
     list_display = ("id","objective_name","objective_descripion", "related_report","objective_color")
     list_filter = ('related_report','objective_color')
     search_fields = ('objective_name','objective_descripion','id')
