8d4a725101e02119d2e0235552cd4f11524722c4
Anastasiya <melichkaowen1@gmail.com>
2024-08-30 20:44:13 +0300

    feat: add profile card

---
+++ application/frontend/src/components/ProfileCard/ProfileCard.styled.ts
@@ -0,0 +1,34 @@
+import { Card } from "@mui/material"
+import { styled } from "@mui/material/styles"
+
+export const ProfileCardContainer = styled(Card)({
+  display: "flex",
+  alignItems: "center",
+  padding: "16px",
+  cursor: "pointer",
+  transition: "0.3s",
+  flexDirection: "row",
+  width: "100%",
+  "&:hover": {
+    boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
+  },
+})
+
+export const Avatar = styled("img")({
+  width: "50px",
+  height: "50px",
+  borderRadius: "50%",
+  marginRight: "16px",
+})
+
+export const ProfileName = styled("div")({
+  flexGrow: 1,
+  fontSize: "18px",
+  fontWeight: "bold",
+})
+
+export const ArrowIcon = styled("div")({
+  marginLeft: "auto",
+  display: "flex",
+  alignItems: "center",
+})
