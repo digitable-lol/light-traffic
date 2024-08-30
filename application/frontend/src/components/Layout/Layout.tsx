cc118a11583cd205478b923e95286cf00f478b00
Anastasiya <melichkaowen1@gmail.com>
2024-08-30 20:57:26 +0300

    fix: fix header display

--- application/frontend/src/components/Layout/Layout.tsx
+++ application/frontend/src/components/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { I18nextProvider } from "react-i18next"
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
 
 import { Header } from "src/components/Header"
 import { Theme } from "src/components/Theme"
@@ -8,13 +8,17 @@ import i18nInstance from "src/locales/service"
 import Markup from "./Layout.styled"
 
 export const Layout = () => {
+  const location = useLocation()
+
+  const shouldShowHeader = location.pathname !== "/"
+
   return (
     <Markup.Container>
       <Theme>
         <Markup.GlobalStyle />
 
         <I18nextProvider i18n={i18nInstance}>
-          <Header />
+          {shouldShowHeader && <Header />}
           <Outlet />
         </I18nextProvider>
       </Theme>
