70df4e22904fb69a78f965cd13485cda964df8eb
Anastasiya <melichkaowen1@gmail.com>
2024-09-06 14:47:47 +0300

    refactor(frontend): delete more button

--- application/frontend/src/components/Header/Header.tsx
+++ application/frontend/src/components/Header/Header.tsx
@@ -96,18 +96,6 @@ export const Header: React.FC = () => {
               <AccountCircleOutlined />
             </IconButton>
           </Tooltip>
-          <Tooltip title={t("more")}>
-            <IconButton
-              sx={{
-                backgroundColor: "rgba(238, 238, 238, 1)",
-                borderRadius: "6px",
-                width: "28px",
-                height: "28px",
-              }}
-            >
-              <MoreVertOutlined />
-            </IconButton>
-          </Tooltip>
         </IconButtons>
 
         <Switches>
